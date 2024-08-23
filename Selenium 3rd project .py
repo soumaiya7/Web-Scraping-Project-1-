282abc40af9bacb5994c4a8aac9490a722ff3bc4
soumaiya7 <soumaiyasoso@yahoo.com>
2024-08-23 21:22:20 +0200

    3rd project with web scraping

---
+++ Selenium 3rd project .py
@@ -0,0 +1,40 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+
+# https://www.geeksforgeeks.org/selenium-python-tutorial/
+# https://www.geeksforgeeks.org/locating-single-elements-in-selenium-python/
+# https://selenium-python.readthedocs.io/locating-elements.html
+
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://authn.edx.org/login")
+driver.implicitly_wait(10)
+
+
+try:
+    cooky_1 = driver.find_element(By.ID, "onetrust-reject-all-handler")
+    cooky_1.click()
+except: 
+    print("No cookies is needed")
+
+email = driver.find_element(By.ID,"emailOrUsername")
+password = driver.find_element(By.ID,"password")
+
+
+email.send_keys("soumaiyaanwerabz@gmail.com")
+password.send_keys("2J?ea_cmJ9EE*P4")
+sing_in = driver.find_element(By.ID, "sign-in")
+sing_in.click()
+
+time.sleep(10)
